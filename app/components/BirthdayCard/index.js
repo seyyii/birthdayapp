283dd29ea0677c795275186bb1d87260eb4c2d19
seyyii <omotoshotoheeb@gmail.com>
2020-03-20 10:57:04 +0100

    structring

--- app/components/BirthdayCard/index.js
+++ app/components/BirthdayCard/index.js
@@ -8,12 +8,7 @@ import Pstyles from './style'
 export default function MyBirthdayCard() {
     return (
         
-            <TouchableHighlight style={Pstyles.button}
-            activeOpacity= {0.85}
-            underlayColor='red'
-                >
-            <Image style={{ width: 160, height: 200, borderRadius: 20 }} source={require('../../assets/birthday.jpg')} />
-            </TouchableHighlight>
-        
+            <Image style={{ width: 160, height: 200, borderRadius: 20 }}{...props} />
+            
     )
 }
---
+++ app/services/ImageData.js
@@ -0,0 +1,6 @@
+const imgData=[
+    {'image': require('../assets/birthdaycard1.png')},
+    { 'image': require('../assets/birthdaycard2.jpg')},
+    { 'image': require('../assets/birthdaycard3.jpg')},
+    { 'image': require('../assets/birthdaycard4.jpg')}
+]
